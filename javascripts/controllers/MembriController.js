2f820a488d64015e079ac6c48f0d764d9f5cc72c
LBindustries <lorenzo.balugani@gmail.com>
2016-11-18 16:58:35 +0100

    Site now online

---
+++ javascripts/controllers/MembriController.js
@@ -0,0 +1,83 @@
+﻿app.controller("MembriController", ['$scope', function($scope)
+{
+	var membrilist = [
+		{
+			name: "Balugani Lorenzo",
+			desc: "Descrizione Qui",
+			icon: "",
+		},
+		{
+			name: "Casari Giovanni",
+			desc: "Descrizione Qui).",
+			icon: "",
+		},
+		{
+			name: "Borghi Martina",
+			desc: "Descrizione Qui",
+			icon: "",
+		},
+		{
+			name: "Torrini Edoardo",
+			desc: "Descrizione Qui",
+			icon: "",
+		},
+		{
+			name: "Pietro",
+			desc: "Descrizione Qui",
+			icon: "",
+		},
+		{
+			name: "Mario Benito Totaro",
+			desc: "Descrizione Qui",
+			icon: "",
+		},
+		{
+			name: "Federico Ipocoana",
+			desc: "Descrizione Qui",
+			icon: "",
+		},
+		{
+			name: "Alexander Magnaye",
+			desc: "Descrizione Qui",
+			icon: "",
+		},
+		{
+			name: "Lupo",
+			desc: "Descrizione Qui",
+			icon: "",
+		},
+		{
+			name: "Marassi Lorena",
+			desc: "Descrizione Qui",
+			icon: "",
+		},
+		{
+			name: "Manzini Emanuela",
+			desc: "Descrizione Qui",
+			icon: "",
+		},
+		{
+			name: "Pigozzi Stefano",
+			desc: "Programmatore, ha creato il css e il sito base dal quale questo si è sviluppato.",
+			icon: "",
+		},
+	]
+	
+	$scope.membri = [];
+	
+	for(var i=0; i < Math.ceil(membrilist.length) / 3; i++)
+	{
+		if(membrilist[i * 3] && membrilist[i * 3 + 1] && membrilist[i * 3 + 2])
+		{
+			$scope.membri[i] = [membrilist[i * 3], membrilist[i * 3 + 1], membrilist[i * 3 + 2]];
+		}
+		else if(membrilist[i * 3] && membrilist[i * 3 + 1])
+		{
+			$scope.membri[i] = [membrilist[i * 3], membrilist[i * 3 + 1]];
+		}
+		else if(membrilist[i * 3])
+		{
+			$scope.membri[i] = [membrilist[i * 3]];
+		}
+	};
+}]);
---
+++ javascripts/controllers/NewsController.js
@@ -0,0 +1,23 @@
+﻿app.controller("NewsController", ['$scope', function ($scope)
+{
+	var newslist = [
+        {
+            time: "2016-11-17",
+            text: "Creazione del sito e degli strumenti Social"
+        },
+	];
+	
+	$scope.events = [
+		{
+            time: "2016-11-23",
+            text: "Prima sessione di lavoro",
+            link: "#"
+		}
+	]
+	
+	$scope.list = [];
+	for(var i=0; i < newslist.length; i++)
+	{
+		$scope.list[i] = newslist[newslist.length - 1 - i];
+	};
+}]);
